eda5e9b8aba1837f4c70aa6d9b9532eed8d16167
Luiz Victor <luizvictornacimento00@gmail.com>
2022-07-06 00:27:13 -0300

    feat: form validation finalized

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -1,26 +1,70 @@
 import {
-	Flex,
 	Box,
+	Button,
+	Flex,
 	FormControl,
+	FormErrorMessage,
 	FormLabel,
+	Heading,
+	HStack,
 	Input,
 	InputGroup,
-	HStack,
 	InputRightElement,
 	Stack,
-	Button,
-	Heading,
 	Text,
 	useColorModeValue,
 	useToast
 } from '@chakra-ui/react';
-import { useState } from 'react';
-import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import {useState} from 'react';
+import {ViewIcon, ViewOffIcon} from '@chakra-ui/icons';
 import {service} from '../../services/service'
+import {Field, Form, Formik} from 'formik';
+import * as yup from 'yup';
 
-export default function FormSignUp() {
+const SignupSchema = yup.object().shape({
+	firstName: yup.string()
+			.min(2, 'Muito Curto')
+			.required('Obrigatório'),
+	lastName: yup.string()
+			.min(2, 'Muito Curto')
+			.required('Obrigatório'),
+	email: yup.string()
+			.email('Email Invalido')
+			.required('Obrigatório'),
+	password: yup.string()
+			.min(6, 'Muito Curto')
+			.required('Obrigatório'),
+	passwordConfirmation: yup.string()
+			.min(6, 'Muito Curto')
+			.oneOf([yup.ref('password'), null], 'Senhas não conferem')
+			.required('Obrigatório'),
+	street: yup.string()
+			.required('Obrigatório'),
+	neighborhood: yup.string()
+			.required('Obrigatório'),
+	number: yup.string()
+			.required('Obrigatório'),
+	complement: yup.string(),
+	city: yup.string()
+			.required('Obrigatório'),
+	state: yup.string()
+			.required('Obrigatório'),
+});
+
+const FormSignUp = () => {
 	const [showPassword, setShowPassword] = useState(false);
-	const [address, setAddress] = useState({});
+	const [address, setAddress] = useState({
+		"cep": "",
+		"logradouro": "",
+		"complemento": "",
+		"bairro": "",
+		"localidade": "",
+		"uf": "",
+		"ibge": "",
+		"gia": "",
+		"ddd": "",
+		"siafi": ""
+	});
 	const toast = useToast()
 
 	const checkCep = async (e) => {
@@ -63,104 +107,189 @@ export default function FormSignUp() {
 									para acessar nossos recursos maneiros ✌️
 								</Text>
 							</Stack>
-							<HStack>
-								<Box>
-									<FormControl id="firstName" isRequired>
-										<FormLabel>Nome</FormLabel>
-										<Input type="text" />
-									</FormControl>
-								</Box>
-								<Box>
-									<FormControl id="lastName">
-										<FormLabel>Sobrenome</FormLabel>
-										<Input type="text" />
-									</FormControl>
-								</Box>
-							</HStack>
-							<FormControl id="email" isRequired>
-								<FormLabel>Email</FormLabel>
-								<Input type="email" />
-							</FormControl>
-							<FormControl id="password" isRequired>
-								<FormLabel>Senha</FormLabel>
-								<InputGroup>
-									<Input type={showPassword ? 'text' : 'password'} />
-									<InputRightElement h={'full'}>
-										<Button
-												variant={'ghost'}
-												onClick={() =>
-														setShowPassword((showPassword) => !showPassword)
-												}>
-											{showPassword ? <ViewIcon /> : <ViewOffIcon />}
-										</Button>
-									</InputRightElement>
-								</InputGroup>
-							</FormControl>
-							<FormControl id="passwordConfirmation" isRequired>
-								<FormLabel>Confirmar Senha</FormLabel>
-								<InputGroup>
-									<Input type={showPassword ? 'text' : 'password'} />
-									<InputRightElement h={'full'}>
-										<Button
-												variant={'ghost'}
-												onClick={() =>
-														setShowPassword((showPassword) => !showPassword)
-												}>
-											{showPassword ? <ViewIcon /> : <ViewOffIcon />}
-										</Button>
-									</InputRightElement>
-								</InputGroup>
-							</FormControl>
-							<HStack>
-								<FormControl id="cep" isRequired>
-									<FormLabel>CEP</FormLabel>
-									<Input type="text" maxLength={8} onChange={(e) => checkCep(e)}/>
-								</FormControl>
-								<FormControl id="street" isDisabled>
-									<FormLabel>Rua</FormLabel>
-									<Input type="text" value={address.logradouro || ''}/>
-								</FormControl>
-							</HStack>
-							<FormControl>
-								<FormLabel>Complemento</FormLabel>
-								<Input type="text" />
-							</FormControl>
-							<HStack>
-								<FormControl id="neighborhood" isDisabled>
-									<FormLabel>Bairro</FormLabel>
-									<Input type="text" value={address.bairro || ''}/>
-								</FormControl>
-								<FormControl id="number">
-									<FormLabel>Número</FormLabel>
-									<Input type="text" />
-								</FormControl>
-							</HStack>
-							<HStack>
-									<FormControl id="city" isDisabled>
-										<FormLabel>Cidade</FormLabel>
-										<Input type="text" value={address.localidade || ''}/>
-									</FormControl>
-								<FormControl id="state" isDisabled>
-									<FormLabel>Estado</FormLabel>
-									<Input type="text" value={address.uf || ''}/>
-								</FormControl>
-							</HStack>
-							<Stack spacing={10} pt={2}>
-								<Button
-										loadingText="Submitting"
-										size="lg"
-										id='submit'
-										bg={'blue.400'}
-										color={'white'}
-										_hover={{
-											bg: 'blue.500',
-										}}>
-									Cadastrar
-								</Button>
-							</Stack>
+							<Formik
+									initialValues={{
+										firstName: '',
+										lastName: '',
+										email: '',
+										password: '',
+										passwordConfirmation: '',
+										street: address.logradouro ? address.logradouro : '',
+										neighborhood: address.bairro ? address.bairro : '',
+										number: '',
+										complement: '',
+										city: address.localidade ? address.localidade : '',
+										state: address.uf ? address.uf : '',
+									}}
+									validationSchema={SignupSchema}
+									enableReinitialize={true}
+									onSubmit={values => {
+										// same shape as initial values
+										console.log(values);
+									}}
+							>
+								{(props) => (
+										<Form>
+											<HStack>
+												<Field name="firstName">
+													{({field, form}) => (
+															<FormControl isRequired
+															             isInvalid={Boolean(form.errors.firstName && form.touched.firstName)}>
+																<FormLabel htmlFor='firstName'>Nome</FormLabel>
+																<Input {...field} type="text" id='firstName' placeholder='Nome'/>
+																<FormErrorMessage>{form.errors.firstName}</FormErrorMessage>
+															</FormControl>
+													)}
+												</Field>
+												<Field name="lastName">
+													{({field, form}) => (
+															<FormControl isRequired
+															             isInvalid={Boolean(form.errors.lastName && form.touched.lastName)}>
+																<FormLabel htmlFor='lastName'>Sobrenome</FormLabel>
+																<Input {...field} type="text" id='lastName' placeholder="Sobrenome"/>
+																<FormErrorMessage>{form.errors.lastName}</FormErrorMessage>
+															</FormControl>
+													)}
+												</Field>
+											</HStack>
+											<Field name='email'>
+												{({field, form}) => (
+														<FormControl isRequired isInvalid={form.errors.email && form.touched.email}>
+															<FormLabel htmlFor='email'>Email</FormLabel>
+															<Input {...field} type="email" id='email' placeholder='Seu melhor email'/>
+															<FormErrorMessage>{form.errors.email}</FormErrorMessage>
+														</FormControl>
+												)}
+											</Field>
+											<Field name='password'>
+												{({field, form}) => (
+														<FormControl isRequired isInvalid={form.errors.password && form.touched.password}>
+															<FormLabel htmlFor='password'>Senha</FormLabel>
+															<InputGroup>
+																<Input {...field} type={showPassword ? 'text' : 'password'} id='password'
+																       placeholder='Senha'/>
+																<InputRightElement h={'full'}>
+																	<Button
+																			variant={'ghost'}
+																			onClick={() =>
+																					setShowPassword((showPassword) => !showPassword)
+																			}>
+																		{showPassword ? <ViewIcon/> : <ViewOffIcon/>}
+																	</Button>
+																</InputRightElement>
+															</InputGroup>
+															<FormErrorMessage>{form.errors.password}</FormErrorMessage>
+														</FormControl>
+												)}
+											</Field>
+											<Field name='passwordConfirmation'>
+												{({field, form}) => (
+														<FormControl isRequired
+														             isInvalid={form.errors.passwordConfirmation && form.touched.passwordConfirmation}>
+															<FormLabel htmlFor='passwordConfirmation'>Confirmar Senha</FormLabel>
+															<InputGroup>
+																<Input {...field} type={showPassword ? 'text' : 'password'} id='passwordConfirmation'
+																       placeholder='Confirme sua senha'/>
+																<InputRightElement h={'full'}>
+																	<Button
+																			variant={'ghost'}
+																			onClick={() =>
+																					setShowPassword((showPassword) => !showPassword)
+																			}>
+																		{showPassword ? <ViewIcon/> : <ViewOffIcon/>}
+																	</Button>
+																</InputRightElement>
+															</InputGroup>
+															<FormErrorMessage>{form.errors.passwordConfirmation}</FormErrorMessage>
+														</FormControl>
+												)}
+											</Field>
+											<HStack>
+												<FormControl id="cep" isRequired>
+													<FormLabel>CEP</FormLabel>
+													<Input type="text" maxLength={8} onChange={(e) => checkCep(e)}/>
+												</FormControl>
+												<Field name="street">
+													{({field, form}) => (
+															<FormControl isDisabled isInvalid={form.errors.street && form.touched.street}>
+																<FormLabel htmlFor='street'>Rua</FormLabel>
+																<Input {...field} type="text" id='street' placeholder='Rua'/>
+																<FormErrorMessage>{form.errors.street}</FormErrorMessage>
+															</FormControl>
+													)}
+												</Field>
+											</HStack>
+											<Field name="complement">
+												{({field, form}) => (
+														<FormControl isInvalid={form.errors.complement && form.touched.complement}>
+															<FormLabel htmlFor='complement'>Complemento</FormLabel>
+															<Input {...field} id='complement' type="text" placeholder='Ex: Prox. Praça da Bandeira'/>
+															<FormErrorMessage>{form.errors.complement}</FormErrorMessage>
+														</FormControl>
+												)}
+											</Field>
+											<HStack>
+												<Field name="neighborhood">
+													{({field, form}) => (
+															<FormControl isDisabled isInvalid={form.errors.neighborhood && form.touched.neighborhood}>
+																<FormLabel htmlFor='neighborhood'>Bairro</FormLabel>
+																<Input {...field} id='neighborhood' type="text" placeholder='Bairro'/>
+																<FormErrorMessage>{form.errors.neighborhood}</FormErrorMessage>
+															</FormControl>
+													)}
+												</Field>
+												<Field name="number">
+													{({field, form}) => (
+															<FormControl isInvalid={form.errors.number && form.touched.number}>
+																<FormLabel htmlFor='number'>Número</FormLabel>
+																<Input {...field} type="text" id='number' placeholder='Ex: 1010'/>
+																<FormErrorMessage>{form.errors.number}</FormErrorMessage>
+															</FormControl>
+													)}
+												</Field>
+											</HStack>
+											<HStack>
+												<Field name="city">
+													{({field, form}) => (
+															<FormControl isDisabled isInvalid={form.errors.city && form.touched.city}>
+																<FormLabel htmlFor='city'>Cidade</FormLabel>
+																<Input {...field} type="text" id="city" placeholder='Cidade'/>
+																<FormErrorMessage>{form.errors.city}</FormErrorMessage>
+															</FormControl>
+													)}
+												</Field>
+												<Field name="state">
+													{({field, form}) => (
+															<FormControl isDisabled isInvalid={form.errors.state && form.touched.state}>
+																<FormLabel htmlFor='state'>Estado</FormLabel>
+																<Input {...field} type="text" id="state" placeholder='Estado'/>
+																<FormErrorMessage>{form.errors.state}</FormErrorMessage>
+															</FormControl>
+													)}
+												</Field>
+											</HStack>
+											<Stack spacing={10} pt={7}>
+												<Button
+														type="submit"
+														loadingText="Submitting"
+														size="lg"
+														id='submit'
+														bg={'blue.400'}
+														color={'white'}
+														_hover={{
+															bg: 'blue.500',
+														}}>
+													Cadastrar
+												</Button>
+											</Stack>
+										</Form>
+								)}
+							</Formik>
 						</Stack>
 					</Box>
 				</Stack>
 			</Flex>
 	);
 }
+
+export default FormSignUp;
